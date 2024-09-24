cead91d83fb0aa0ce904e8bc614c376b1d7862b2
manuac777 <manuelacuna78398@gmail.com>
2024-09-23 19:10:03 -0600

    Actualizacion

--- Problema1.py
+++ Problema1.py
@@ -1,11 +1,9 @@
 from collections import Counter
-while True:
-    S = input("")
-    if S == "0":
-        break
-    X = input("")
+
+def validate_Posible(S,X):
     cuenta_S = Counter(S)
     cuenta_X = Counter(X)
+
     es_posible = True
     for letra in X:
         if cuenta_S[letra] > cuenta_X[letra]: 
@@ -16,6 +14,37 @@ while True:
     else:
         print("IMPOSIBLE")
 
+
+while True:
+    S = input("")
+    if S == "0":
+        break
+    X = input("")
+    if X == '0' :
+        break
+
+    char_delete = input()
+
+    if(char_delete) :
+        auxS = S.replace(char_delete, "")
+        cuenta_auxS = Counter(auxS)
+        cuenta_X = Counter(X)
+
+        es_posible = True
+        for letra in X:
+            if cuenta_auxS[letra] < cuenta_X[letra]: 
+                es_posible = False
+                break
+        if es_posible:
+            print(auxS)
+            print("POSIBLE")
+        else:
+            print(auxS)
+            print("IMPOSIBLE")
+
+    else :
+        validate_Posible(S,X)
+        
 #--------Funcionamiento de la libreria----------------------     
 #import collections
 #print collections.Counter(['a', 'b', 'c', 'a', 'b', 'b'])
